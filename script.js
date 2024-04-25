8a5ac2fd19ee683ff87a16b3c63b8c3e2ff7ae4c
Ashar Khan <asharullah.khan2468@gmail.com>
2024-04-25 22:04:49 +0500

    Stopwatch

---
+++ script.js
@@ -0,0 +1,16 @@
+let second=document.querySelector("#h3");
+
+
+// function increaseNum() {
+//     let sec=0;
+//     setInterval(function () {
+//       sec+=1;
+//       second.innerHTML=sec
+//     } , 1000)
+//     setTimeout();
+   
+// } 
+
+
+// setTimeout(function(){console.log("Hello World") , 1000;}
+// );
